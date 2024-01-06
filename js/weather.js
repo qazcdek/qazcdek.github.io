97f5467ba000b181e9fadd5281de946a480d24f8
all4boodi <146738453+all4boodi@users.noreply.github.com>
2024-01-07 07:38:13 +0900

    add location

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,12 @@
 const API_KEY = "7e5206aa55c5d1b078bfb7b898fbbb5c";
-const weatherIcon = document.querySelector("#topbar__weather img:first-child");
+const weatherIcon = document.querySelector(".topbar__weather-icon");
 const weatherTempMin = document.querySelector(
-  "#topbar__weather > div > span:first-child"
+  "#topbar__weather > div:last-child > span:first-child"
 );
 const weatherTempMax = document.querySelector(
-  "#topbar__weather > div > span:last-child"
+  "#topbar__weather > div:last-child > span:last-child"
 );
+const weatherLocation = document.querySelector(".topbar__weather-loc");
 
 function onGeoOK(position) {
   const lat = position.coords.latitude;
@@ -16,11 +17,13 @@ function onGeoOK(position) {
     .then((data) => {
       const tempMin = data.main.temp_min;
       const tempMax = data.main.temp_max;
+      const presentLocation = data.name;
       const icon = data.weather[0].icon;
       const urlIcon = `https://openweathermap.org/img/wn/${icon}@2x.png`;
       weatherIcon.src = urlIcon;
       weatherTempMin.innerText = `최저:  ${Math.round(10 * tempMin) / 10} °C`;
       weatherTempMax.innerText = `최고:  ${Math.round(10 * tempMax) / 10} °C`;
+      weatherLocation.innerText = `${presentLocation}`;
       console.log(typeof tempMax);
     });
 }
